846442090188f7c1a76b328e5ea493c432337845
sixbeeshades <abhijith.p.subash@gmail.com>
2022-09-13 22:44:59 +0530

    change

--- src/app/core/api/api.service.ts
+++ src/app/core/api/api.service.ts
@@ -10,38 +10,33 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(
-    private http: HttpClient, 
-    private jwtService: JwtService
-    ) {}
+  constructor(private http: HttpClient, private jwtService: JwtService) {}
 
-    private formatErrors(error:any){
-      return throwError(error.error);
-    }
+  private formatErrors(error: any) {
+    return throwError(error.error);
+  }
 
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http
+      .get(`${environment.api_url}${path}`, { params })
+      .pipe(catchError(this.formatErrors));
+  }
 
-    get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
-      return this.http.get(`${environment.api_url}${path}`, { params })
-        .pipe(catchError(this.formatErrors));
-    }
-  
-    put(path: string, body: Object = {}): Observable<any> {
-      return this.http.put(
-        `${environment.api_url}${path}`,
-        JSON.stringify(body)
-      ).pipe(catchError(this.formatErrors));
-    }
-  
-    post(path: string, body: Object = {}): Observable<any> {
-      return this.http.post(
-        `${environment.api_url}${path}`,
-        JSON.stringify(body)
-      ).pipe(catchError(this.formatErrors));
-    }
-  
-    delete(path: any): Observable<any> {
-      return this.http.delete(
-        `${environment.api_url}${path}`
-      ).pipe(catchError(this.formatErrors));
-    }
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http
+      .put(`${environment.api_url}${path}`, JSON.stringify(body))
+      .pipe(catchError(this.formatErrors));
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http
+      .post(`${environment.api_url}${path}`, JSON.stringify(body))
+      .pipe(catchError(this.formatErrors));
+  }
+
+  delete(path: any): Observable<any> {
+    return this.http
+      .delete(`${environment.api_url}${path}`)
+      .pipe(catchError(this.formatErrors));
+  }
 }
